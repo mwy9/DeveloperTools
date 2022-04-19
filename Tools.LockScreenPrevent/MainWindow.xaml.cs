d00c6c9815bedb2c51f319c1bc5605efcd15e72b
Administrator <Administrator@DESKTOP-LELE76P>
2022-04-19 12:41:18 +0800

    添加项目文件。

---
+++ Tools.LockScreenPrevent/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LockScreenPrevent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer movementTimer = new DispatcherTimer();
+        private int PointLocationX = 10;
+        private int PointLocationY = 10;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            movementTimer.Tick += movementTimer_Tick;
+            movementTimer.Interval = TimeSpan.FromSeconds(200);
+            
+        }
+
+        private void btnTimerStart_Click(object sender, RoutedEventArgs e)
+        {
+            movementTimer.Start();
+        }
+
+        private void btnTimerStop_Click(object sender, RoutedEventArgs e)
+        {
+            movementTimer.Stop();
+        }
+
+
+        //Tick:定时器,每当经过多少时间发生函数
+        private void movementTimer_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            var flag = random.Next(30, 300);
+
+            int stepx = PointLocationX + flag;
+            int stepy = PointLocationY + flag;
+
+            SetCursorPos(stepx, stepy);
+            mouse_event(MouseEventFlag.RightDown, 0, 0, 0, UIntPtr.Zero);
+            mouse_event(MouseEventFlag.RightUp, 0, 0, 0, UIntPtr.Zero);
+            mouse_event(MouseEventFlag.Move, stepx, stepy, 0, UIntPtr.Zero);
+        }
+
+
+        //结构体布局 本机位置
+        [StructLayout(LayoutKind.Sequential)]
+        struct NativeRECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        //将枚举作为位域处理
+        [Flags]
+        enum MouseEventFlag : uint //设置鼠标动作的键值
+        {
+            Move = 0x0001,               //发生移动
+            LeftDown = 0x0002,           //鼠标按下左键
+            LeftUp = 0x0004,             //鼠标松开左键
+            RightDown = 0x0008,          //鼠标按下右键
+            RightUp = 0x0010,            //鼠标松开右键
+            MiddleDown = 0x0020,         //鼠标按下中键
+            MiddleUp = 0x0040,           //鼠标松开中键
+            XDown = 0x0080,
+            XUp = 0x0100,
+            Wheel = 0x0800,              //鼠标轮被移动
+            VirtualDesk = 0x4000,        //虚拟桌面
+            Absolute = 0x8000
+        }
+
+        //设置鼠标位置
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        //设置鼠标按键和动作
+        [DllImport("user32.dll")]
+        static extern void mouse_event(MouseEventFlag flags, int dx, int dy,
+            uint data, UIntPtr extraInfo); //UIntPtr指针多句柄类型
+
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string strClass, string strWindow);
+
+        //该函数获取一个窗口句柄,该窗口雷鸣和窗口名与给定字符串匹配 hwnParent=Null从桌面窗口查找
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter,
+            string strClass, string strWindow);
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(HandleRef hwnd, out NativeRECT rect);
+
+        
+    }
+}
